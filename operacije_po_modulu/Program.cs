9e735d7c36de211566d5728523a6c9b4f7e8a934
usern4mehere9 <91462628+usern4mehere9@users.noreply.github.com>
2021-10-02 18:42:10 +0200

    Add files via upload

---
+++ operacije_po_modulu/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace operacije_po_modulu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long m, n, o, p, z, pr, zz, zpr, a, b;
+
+            m = long.Parse(Console.ReadLine());
+            n = long.Parse(Console.ReadLine());
+            o = long.Parse(Console.ReadLine());
+            p = long.Parse(Console.ReadLine());
+
+            z = m + n + o + p;
+            pr = m * n * o * p;
+
+            a = z % 1000;
+            b = pr % 1000;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
